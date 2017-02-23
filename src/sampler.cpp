ad046556d66bebd11c7d567b0a9b3bb69cb598bf
Roland Putnoki <roland.putnoki95@gmail.com>
2017-02-23 14:08:44 +0100

    "arc sampling final"

--- src/sampler.cpp
+++ src/sampler.cpp
@@ -10,6 +10,11 @@
 #include "path_sampler/Sampled_path.h"
 #include <math.h>
 
+#include <visualization_msgs/Marker.h>
+#include <cmath>
+
+
+
 using namespace robot_editor;
 using namespace path_sampler;
 
@@ -58,6 +63,28 @@ void line_sampler(ConfigInterval c, Sampled_path & sp){
             phi = 180 - alfa;
         }
     }
+
+
+    ros::NodeHandle n2;
+    ros::Publisher marker_pub = n2.advertise<visualization_msgs::Marker>("visualization_marker", 10);
+    visualization_msgs::Marker points, line_strip, line_list;
+    points.header.frame_id = line_strip.header.frame_id = line_list.header.frame_id = "/my_frame";
+    points.header.stamp = line_strip.header.stamp = line_list.header.stamp = ros::Time::now();
+    points.ns = line_strip.ns = line_list.ns = "points_and_lines";
+    points.action = line_strip.action = line_list.action = visualization_msgs::Marker::ADD;
+    points.pose.orientation.w = line_strip.pose.orientation.w = line_list.pose.orientation.w = 1.0;
+
+    points.id = 0;
+    points.type = visualization_msgs::Marker::POINTS;
+    // POINTS markers use x and y scale for width/height respectively
+    points.scale.x = 0.2;
+    points.scale.y = 0.2;
+    // Points are green
+    points.color.g = 1.0;
+    points.color.a = 1.0;
+
+
+
     for(int i = 0; i <= num_of_points; i++){
         double x = c.start.x + i*delta_x;
         double y = c.start.y + i*delta_y;
@@ -66,7 +93,20 @@ void line_sampler(ConfigInterval c, Sampled_path & sp){
         conf.y = y;
         conf.phi = phi;
         sp.sample_points.push_back(conf);
+
+        geometry_msgs::Point p;
+        p.x = x;
+        p.y = y;
+        p.z = 0;
+        points.points.push_back(p);
     }
+    geometry_msgs::Point p;
+    p.x = 10;
+    p.y = 10;
+    p.z = 10;
+    points.points.push_back(p);
+    marker_pub.publish(points);
+    ros::spin();
 }
 
 void arc_sampler(ConfigInterval c, Sampled_path & sp){
@@ -80,19 +120,24 @@ void arc_sampler(ConfigInterval c, Sampled_path & sp){
     double alfa = c.arc_start;    //x-tengellyel bezárt szög
     double delta_arc = fabs(c.delta)/arc_resolution;    //Lépésköz szögben
     double diff_from_start = 0;     //A körív kezdetétől mennyire vagyunk
-    double delta_x;
-    double delta_y;
+    double delta_x;         //A körív adott pontjának x koordinátájának eltérése a kör kp.jának x koordinátájától
+    double delta_y;         //A körív adott pontjának y koordinátájának eltérése a kör kp.jának y koordinátájától
     double phi;
     while(diff_from_start <= fabs(c.delta)){
+        //1. síknegyed
         if(alfa <= 90){
             delta_y = sin(alfa)*radius;
             delta_x = cos(alfa)*radius;
+            //Egy irányba néz az autó, ha óra mutatóval szemben előre halad, vagy ha óra mutatóval megegyezően tolat
+            //és ugyanez fordítva is igaz
             if((c.delta > 0 && c.orientation != 0) || (c.delta < 0 && c.orientation == 0) ){
                 phi = alfa + 90;
             } else{
                 phi = alfa + 270;
             }
-        } else if(alfa <= 180){
+        }
+        //2. síknegyed
+        else if(alfa <= 180){
             delta_y = sin(180-alfa)*radius;
             delta_x = -cos(180-alfa)*radius;
             if((c.delta > 0 && c.orientation != 0) || (c.delta < 0 && c.orientation == 0) ){
@@ -100,17 +145,22 @@ void arc_sampler(ConfigInterval c, Sampled_path & sp){
             } else{
                 phi = alfa - 90;
             }
-        } else if(alfa <= 270){
-            delta_y = -sin(alfa - 180)*radius;
-            delta_x = -cos(alfa - 180)*radius;
+        }
+        //3. síknegyed
+        else if(alfa <= 270){
+            delta_y = -sin(270 - alfa)*radius;
+            delta_x = -cos(270 - alfa)*radius;
             if((c.delta > 0 && c.orientation != 0) || (c.delta < 0 && c.orientation == 0) ){
                 phi = alfa + 90;
             } else{
                 phi = alfa - 90;
             }
-        } else {
-            delta_y = -sin(270-alfa)*radius;
-            delta_x = cos(270-alfa)*radius;
+        }
+        //4. síknegyed
+        else {
+            delta_y = -sin(alfa - 270)*radius;
+            delta_x = cos(alfa - 270)*radius;
+
             if((c.delta > 0 && c.orientation != 0) || (c.delta < 0 && c.orientation == 0) ){
                 phi = alfa - 270;
             } else{
@@ -156,6 +206,11 @@ void pathCallback(const Path p)
             ROS_INFO("Ez egy koriv");
         }
     }
+
+    ros::NodeHandle n_publisher;
+    ros::Publisher sampled_pub = n_publisher.advertise<Sampled_path>("sampled", 1000);
+    sampled_pub.publish(sample);
+    ros::spin();
 }
 
 int main(int argc, char **argv){
